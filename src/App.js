21b9c65ede0042b05d088c2b09c5d957c18f61c4
Brianna Renni <briannaxrene@icloud.com>
2023-02-26 03:01:50 -0800

    rename: AppHeader -> AppNav

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import { AuthProvider } from './hooks';
-import { Navbar, NotFound } from './components/ui';
+import { AppNav, NotFound } from './components/ui';
 import { BackBtn } from './components/btn/';
 import { Landing, FAQ, Login, Register, Dashboard } from './pages';
 
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <AuthProvider>
       <div className="App container-fluid">
-        <Navbar />
+        <AppNav />
         <BackBtn />
         <Routes>
           <Route
--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,4 +1,4 @@
-export { default as Navbar } from './Navbar';
+export { default as AppNav } from './AppNav';
 export { default as NotFound } from './NotFound';
 export { default as UXAlert } from './UXAlert';
 export { default as PageHeader } from './PageHeader';
